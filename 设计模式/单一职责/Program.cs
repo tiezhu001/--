07b74d08bc05809d9cf22b0293861271ed1d9fc3
tiezhu001 <szh19961117@163.com>
2020-01-01 22:23:32 +0800

    1

---
+++ 设计模式/单一职责/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 单一职责
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
